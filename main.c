6f3bfa555b7a033daac40928a020e7a9d48be8ac
hnb <hnandomachi@gmail.com>
2021-03-27 10:41:48 +0700

    ADC dung chan

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 // voltage-sensor  --> ADCA1
 // xuat theta      --> ADAA0
 
+// Signal          Acquisition (ns)        Cycles          ACQPS       ADC PIN     CHSEL
+// Vg              200                     200/5=40        40-1=39     ADCINA0     0
+// Ig              200                     200/5=40        40-1=39     ADCINA1     1
+// Vpv             200                     200/5=40        40-1=39     ADCINB0     0
+// Ipv             200                     200/5=40        40-1=39     ADCINB1     1
+
 #include "F28x_Project.h"
 #include "Solar_F.h"
 
@@ -27,6 +33,16 @@ Uint16 EPwm6_DB_Direction;
 
 SPLL_1ph_SOGI_F spll1;
 
+Uint16 VgSample;
+Uint16 IgSample;
+Uint16 VpvSample;
+Uint16 IpvSample;
+
+float32 Vg;
+float32 Ig;
+float32 Vpv;
+float32 Ipv;
+
 float GrisMeas = 0;
 float index = 0, m = 0.8;
 int role = 1;
@@ -36,7 +52,9 @@ int role = 1;
 //
 void Gpio_setup1(void);
 void InitEPwm2Example(void);
+void ConfigureADC(void);
 void configureDAC(void);
+void SetupADCEpwm(void);
 
 __interrupt void cpu_timer0_isr(void);
 __interrupt void epwm2_isr(void);
@@ -114,6 +132,16 @@ void main(void)
     EDIS;
 
     //
+    // Configure the ADC and power it up
+    //
+    ConfigureADC();
+
+    //
+    // Setup the ADC for ePWM triggered conversions on sensor
+    //
+    SetupADCEpwm();
+
+    //
     // Step 3. Initialize the Device Peripheral. This function can be
     //         found in F2837xS_CpuTimers.c
     //
@@ -175,9 +203,123 @@ void main(void)
     SPLL_1ph_SOGI_F_init(GRID_FREQ, ((float)(1.0 / ISR_FREQUENCY)), &spll1);
     SPLL_1ph_SOGI_F_coeff_update(((float)(1.0 / ISR_FREQUENCY)), (float)(2 * PI * GRID_FREQ), &spll1);
 
-    while (1)
+    do
     {
-    }
+        //
+        //convert, wait for completion, and store results
+        //start conversions immediately via software, ADCA
+        //
+        AdcaRegs.ADCSOCFRC1.all = 0x0003; //SOC0 and SOC1
+
+        //
+        //start conversions immediately via software, ADCB
+        //
+        AdcbRegs.ADCSOCFRC1.all = 0x0003; //SOC0 and SOC1
+
+        //
+        //wait for ADCA to complete, then acknowledge flag
+        //
+        while (AdcaRegs.ADCINTFLG.bit.ADCINT1 == 0)
+            ;
+        AdcaRegs.ADCINTFLGCLR.bit.ADCINT1 = 1;
+
+        //
+        //wait for ADCB to complete, then acknowledge flag
+        //
+        while (AdcbRegs.ADCINTFLG.bit.ADCINT1 == 0)
+            ;
+        AdcbRegs.ADCINTFLGCLR.bit.ADCINT1 = 1;
+
+        //
+        //store results
+        //
+        VgSample = AdcaResultRegs.ADCRESULT0;
+        IgSample = AdcaResultRegs.ADCRESULT1;
+        VpvSample = AdcbResultRegs.ADCRESULT0;
+        IpvSample = AdcbResultRegs.ADCRESULT1;
+
+        //
+        //at this point, conversion results are stored in
+        //AdcaResult0, AdcaResult1, AdcbResult0, and AdcbResult1
+        //
+
+        //
+        //software breakpoint, hit run again to get updated conversions
+        //
+
+    } while (1);
+}
+
+//
+// ConfigureADC - Write ADC configurations and power up the ADC for both
+//                ADC A and ADC B
+//
+void ConfigureADC(void)
+{
+    EALLOW;
+
+    //
+    //write configurations
+    //
+    AdcaRegs.ADCCTL2.bit.PRESCALE = 6; //set ADCCLK divider to /4
+    AdcbRegs.ADCCTL2.bit.PRESCALE = 6; //set ADCCLK divider to /4
+
+    AdcSetMode(ADC_ADCA, ADC_RESOLUTION_12BIT, ADC_SIGNALMODE_SINGLE);
+    AdcSetMode(ADC_ADCB, ADC_RESOLUTION_12BIT, ADC_SIGNALMODE_SINGLE);
+
+    //
+    //Set pulse positions to late
+    //
+    AdcaRegs.ADCCTL1.bit.INTPULSEPOS = 1;
+    AdcbRegs.ADCCTL1.bit.INTPULSEPOS = 1;
+
+    //
+    //power up the ADC
+    //
+    AdcaRegs.ADCCTL1.bit.ADCPWDNZ = 1;
+    AdcbRegs.ADCCTL1.bit.ADCPWDNZ = 1;
+
+    //
+    //delay for 1ms to allow ADC time to power up
+    //
+    DELAY_US(1000);
+
+    EDIS;
+}
+
+//
+// SetupADCEpwm - Configure ADC EPWM acquisition window and trigger
+//
+void SetupADCEpwm(void)
+{
+    Uint16 sensor_acqps = 39;
+
+    //
+    //Select the channels to convert and end of conversion flag
+    //
+
+    // "TRIGSEL" search google
+    EALLOW;
+    // ADCA: Vg, Ig
+    AdcaRegs.ADCSOC0CTL.bit.CHSEL = 0;            //SOC0 will convert internal //connection A0
+    AdcaRegs.ADCSOC0CTL.bit.ACQPS = sensor_acqps; //sample window is 39 ~ 200 (ns) SYSCLK cycles
+    AdcaRegs.ADCSOC1CTL.bit.CHSEL = 1;            //SOC1 will convert internal //connection A1
+    AdcaRegs.ADCSOC1CTL.bit.ACQPS = sensor_acqps; //sample window is 39 ~ 200 (ns) SYSCLK cycles
+
+    AdcaRegs.ADCINTSEL1N2.bit.INT1SEL = 1; //end of SOC1 will set INT1 flag
+    AdcaRegs.ADCINTSEL1N2.bit.INT1E = 1;   //enable INT1 flag
+    AdcaRegs.ADCINTFLGCLR.bit.ADCINT1 = 1; //make sure INT1 flag is cleared
+
+    // ADCB: Vpv, Ipv
+    AdcbRegs.ADCSOC0CTL.bit.CHSEL = 0;            //SOC0 will convert internal //connection B0
+    AdcbRegs.ADCSOC0CTL.bit.ACQPS = sensor_acqps; //sample window is 39 ~ 200 (ns) SYSCLK cycles
+    AdcbRegs.ADCSOC1CTL.bit.CHSEL = 1;            //SOC1 will convert internal //connection B1
+    AdcbRegs.ADCSOC1CTL.bit.ACQPS = sensor_acqps; //sample window is 39 ~ 200 (ns) SYSCLK cycles
+
+    AdcbRegs.ADCINTSEL1N2.bit.INT1SEL = 1; //end of SOC1 will set INT1 flag
+    AdcbRegs.ADCINTSEL1N2.bit.INT1E = 1;   //enable INT1 flag
+    AdcbRegs.ADCINTFLGCLR.bit.ADCINT1 = 1; //make sure INT1 flag is cleared
+    EDIS;
 }
 
 void Gpio_setup1(void)
